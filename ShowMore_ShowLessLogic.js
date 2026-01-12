77709800c2e4c88129f3ddce9f79ef4bd9306771
Jinesh R <146803233+Jineshraj@users.noreply.github.com>
2026-01-12 23:59:02 +0530

    Update ShowMore_ShowLessLogic.js

--- ShowMore_ShowLessLogic.js
+++ ShowMore_ShowLessLogic.js
@@ -1,1 +1,7 @@
 
+const filteredMovies = activeCategory === 'all'
+    ? movies
+    : movies.filter(movie => movie.category === activeCategory);
+  const COLLAPSE_COUNT = 12;
+
+  const visibleMovies = showAll ? filteredMovies : filteredMovies.slice(0, COLLAPSE_COUNT);
